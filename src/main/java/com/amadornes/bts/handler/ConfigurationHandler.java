f6905ca840080b95e24056a10414b7580dad3ab4
Frederik <girafi0@gmail.com>
2014-09-04 04:13:18 +0200

    New feature done.
    Added fully customizable title screen.

--- src/main/java/com/amadornes/bts/handler/ConfigurationHandler.java
+++ src/main/java/com/amadornes/bts/handler/ConfigurationHandler.java
@@ -12,16 +12,16 @@ import static net.minecraftforge.common.config.Configuration.*;
 public class ConfigurationHandler
 {
     public static Configuration configuration;
-    public static String CATEGORY_CUSTOM_TEXT="Custom Text";
+    public static String CATEGORY_CUSTOM_TEXT = "Custom Text";
     public static String[] TitleScreenText;
-    public static boolean TitleScreenMods;
-    public static boolean TitleScreenForge;
-    public static boolean TitleScreenFML;
+    public static boolean TitleScreenMCVersion;
     public static boolean TitleScreenMCP;
+    public static boolean TitleScreenFML;
+    public static boolean TitleScreenForge;
+    public static boolean TitleScreenMods;
 
     public static void init(File configFile)
     {
-        // Create the configuration object from the given configuration file
         if (configuration == null)
         {
             configuration = new Configuration(configFile);
@@ -32,10 +32,11 @@ public class ConfigurationHandler
     private static void loadConfiguration()
     {
         TitleScreenText = configuration.getStringList("Text", CATEGORY_CUSTOM_TEXT, new String[]{"§4B§6E§eT§2T§3E§1R §5T§4I§6T§eL§2E §3S§1C§5R§4E§6E§eN"}, "Insert the text you want here.(Supports multiple lines)");
-        TitleScreenMods = configuration.getBoolean("Show mods loaded", CATEGORY_GENERAL, true, "Show how many mods is loaded(W.I.P.)");
-        TitleScreenForge = configuration.getBoolean("Show Forge version", CATEGORY_GENERAL, false, "Show which version of Forge the client is running.(W.I.P.)");
-        TitleScreenFML = configuration.getBoolean("Show FML version", CATEGORY_GENERAL, false, "Show which version of FML the client is running.(W.I.P.)");
-        TitleScreenMCP = configuration.getBoolean("Show MCP version", CATEGORY_GENERAL, false, "Show which version of MCP the client is running.(W.I.P.)");
+        TitleScreenMCVersion = configuration.getBoolean("Show MC version", CATEGORY_GENERAL, true, "Show which version of Minecraft the client is currently running.");
+        TitleScreenMCP = configuration.getBoolean("Show MCP version", CATEGORY_GENERAL, false, "Show which version of Minecraft Coder Pack (MCP) the client is running.");
+        TitleScreenFML = configuration.getBoolean("Show FML version", CATEGORY_GENERAL, false, "Show which version of Forge Mod Loader (FML) the client is running.");
+        TitleScreenForge = configuration.getBoolean("Show Forge version", CATEGORY_GENERAL, false, "Show which version of Minecraft Forge the client is running.");
+        TitleScreenMods = configuration.getBoolean("Show mods loaded", CATEGORY_GENERAL, true, "Show how many mods is loaded.");
 
         if (configuration.hasChanged())
         {
--- src/main/java/com/amadornes/bts/handler/TitleScreenHandler.java
+++ src/main/java/com/amadornes/bts/handler/TitleScreenHandler.java
@@ -20,54 +20,35 @@ public class TitleScreenHandler{
                 FMLCommonHandler.instance().computeBranding();
                 List<String> brands = new ArrayList<String>((List<String>) f.get(FMLCommonHandler.instance()));
                 List<String> newBrands = new ArrayList<String>();
-                newBrands.add(brands.get(0));
-
-                Collections.addAll(newBrands, TitleScreenText);
 
                 f.set(FMLCommonHandler.instance(), newBrands);
 
-                if (TitleScreenMCP == true)
-                    try {
-                        newBrands.add(brands.get(brands.size() - 4));
+                if (TitleScreenMCVersion == true)
+                    {
+                        newBrands.add(brands.get(0));
                     }
-                    finally {
-                        if (TitleScreenMCP == false)
-                             {
-                                newBrands.add(brands.get(0));
-                            }
+
+                Collections.addAll(newBrands, TitleScreenText);
+
+                if (TitleScreenMCP == true)
+                    {
+                        newBrands.add(brands.get(brands.size() - 4));
                     }
 
                 if (TitleScreenFML == true)
-                    try {
+                    {
                         newBrands.add(brands.get(brands.size() - 3));
-                    }
-                    finally {
-                        if (TitleScreenFML == false)
-                            {
-                               newBrands.add(brands.get(0));
-                            }
                     }
 
                 if (TitleScreenForge == true)
-                    try {
+                    {
                         newBrands.add(brands.get(brands.size() - 2));
                     }
-                    finally {
-                        if (TitleScreenForge == false)
-                        {
-                            newBrands.add(brands.get(0));
-                        }
-                    }
+
 
                 if (TitleScreenMods == true)
-                    try {
+                    {
                         newBrands.add(brands.get(brands.size() - 1));
-                    }
-                    finally {
-                        if (TitleScreenMods == false)
-                        {
-                            newBrands.add(brands.get(0));
-                        }
                     }
 
             } catch (Exception e) {
