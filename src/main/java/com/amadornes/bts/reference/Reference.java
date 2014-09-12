15c3d674efd2c40cdf7a1d69cce2648133ea2249
Frederik <girafi0@gmail.com>
2014-09-12 18:22:20 +0200

    Updated mcmod.info + bumped version to 1.2

--- src/main/java/com/amadornes/bts/reference/Reference.java
+++ src/main/java/com/amadornes/bts/reference/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
     public static final String MOD_ID = "BetterTitleScreen";
     public static final String MOD_NAME = "Better Title Screen";
-    public static final String MOD_VERSION = "1.7.10-1.1";
+    public static final String MOD_VERSION = "1.7.10-1.2";
     public static final String GUI_FACTORY_CLASS = "com.amadornes.bts.client.gui.GuiFactory";
     public static final String CLIENT_PROXY_CLASS = "com.amadornes.bts.proxy.ClientProxy";
     public static final String DEPENDENCIES = "required-after:Forge@[10.12.2.1147,)";
