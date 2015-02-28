2a9ced4bd7860cef31b1b59260ca6cd61d5d412a
Frederik <girafi0@gmail.com>
2015-02-28 11:01:02 +0100

    Improved build.gradle Did some cleanup

--- src/main/java/com/amadornes/bts/reference/Reference.java
+++ src/main/java/com/amadornes/bts/reference/Reference.java
@@ -4,8 +4,8 @@ public class Reference
 {
     public static final String MOD_ID = "BetterTitleScreen";
     public static final String MOD_NAME = "Better Title Screen";
-    public static final String MOD_VERSION = "1.7.10-1.2";
+    public static final String MOD_VERSION = "@VERSION@";
     public static final String GUI_FACTORY_CLASS = "com.amadornes.bts.client.gui.GuiFactory";
     public static final String CLIENT_PROXY_CLASS = "com.amadornes.bts.proxy.ClientProxy";
-    public static final String DEPENDENCIES = "required-after:Forge@[10.13.2.1230,)";
+    public static final String DEPENDENCIES = "required-after:Forge@[10.13.2.1291,)";
 }
