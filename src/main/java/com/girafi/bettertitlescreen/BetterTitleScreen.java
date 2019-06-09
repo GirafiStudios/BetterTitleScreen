ebe47e8eb2bcb306e957bb83f6c9ac0b597505e0
GirafiStudios <faj10@msn.com>
2019-06-09 19:41:18 +0200

    Initial 1.14.2 work

--- src/main/java/com/girafi/bettertitlescreen/BetterTitleScreen.java
+++ src/main/java/com/girafi/bettertitlescreen/BetterTitleScreen.java
@@ -2,15 +2,15 @@ package com.girafi.bettertitlescreen;
 
 import com.girafi.bettertitlescreen.handler.ConfigurationHandler;
 import com.girafi.bettertitlescreen.handler.TitleScreenHandler;
-import com.girafi.bettertitlescreen.reference.Reference;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod(Reference.MOD_ID)
+@Mod(BetterTitleScreen.MOD_ID)
 public class BetterTitleScreen {
+    public static final String MOD_ID = "bettertitlescreen";
 
     public BetterTitleScreen() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::loadComplete);
--- src/main/java/com/girafi/bettertitlescreen/reference/Reference.java
+++
@@ -1,6 +0,0 @@
-package com.girafi.bettertitlescreen.reference;
-
-public class Reference {
-    public static final String MOD_ID = "bettertitlescreen";
-    public static final String GUI_FACTORY_CLASS = "com.girafi.bettertitlescreen.client.gui.GuiFactory";
-}
