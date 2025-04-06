f82668f426920ea5805feb22badee48d115df833
GirafiStudios <faj10@msn.com>
2025-04-06 23:52:20 +0200

    Updated to 1.21.5

--- fabric/src/main/java/com/girafi/bettertitlescreen/BetterTitleScreen.java
+++ fabric/src/main/java/com/girafi/bettertitlescreen/BetterTitleScreen.java
@@ -1,7 +1,7 @@
 package com.girafi.bettertitlescreen;
 
 import com.girafi.bettertitlescreen.handler.ConfigurationHandler;
-import fuzs.forgeconfigapiport.fabric.api.neoforge.v4.NeoForgeConfigRegistry;
+import fuzs.forgeconfigapiport.fabric.api.v5.ConfigRegistry;
 import net.fabricmc.api.ClientModInitializer;
 import net.neoforged.fml.config.ModConfig;
 
@@ -9,6 +9,6 @@ public class BetterTitleScreen implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        NeoForgeConfigRegistry.INSTANCE.register(Constants.MOD_ID, ModConfig.Type.CLIENT, ConfigurationHandler.spec);
+        ConfigRegistry.INSTANCE.register(Constants.MOD_ID, ModConfig.Type.CLIENT, ConfigurationHandler.spec);
     }
 }
